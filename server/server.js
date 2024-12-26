97a39f8a1514b1dde57e07058097a93de8949e39
unknown <amweezy2@gmail.com>
2024-12-26 15:43:10 +0300

    database connection added with dontenv

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config();
 
 // Basic route to handle requests
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('databased connection established');
+  })
+  .catch(err => {
+    console.error('database connection error:', err);
+  });
 
-const { getPreviousGrade } = require('./utils/utils'); // Import the function
-console.log(getPreviousGrade('PRM_MID', 4, 'GEN')); 
+// Start the server
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${process.env.SERVER_PORT}`);
+});
